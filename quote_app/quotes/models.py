34d3d6038cbefd365a1bd4e674774c78a3abbe04
Ibrahim <Selmentausen@gmail.com>
2025-07-26 10:42:18 +0300

    Setup quotes models

--- quote_app/quotes/models.py
+++ quote_app/quotes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+class Source(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Quote(models.Model):
+    text = models.TextField(unique=True)  # made unique to prevent duplicates
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    weight = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.text
+
+
+class ViewCounter(models.Model):
+    views = models.PositiveIntegerField(default=0)
